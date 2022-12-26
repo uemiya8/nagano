c9bde290716c478fe616fcfb9350f98c40a4e1b9
EC2 Default User <ec2-user@ip-172-31-33-173.ap-northeast-1.compute.internal>
2022-12-26 14:57:29 +0000

    Eighth

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,4 +12,9 @@ class Admin::CustomersController < ApplicationController
    @customer = Customer.find(params[:id])
   end
   
+  private
+  
+  def customer_params
+   params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana, :postal_code, :address,)
+  end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,6 +13,12 @@ class Admin::ItemsController < ApplicationController
   @item = Item.find(params[:id])
  end
  
+ def update
+  @item = Item.find(params[:id])
+   @item.update(item_params)
+   redirect_to admin_item_path(@item.id)
+ end
+ 
  def create
   item = Item.new(item_params)
   item.save
